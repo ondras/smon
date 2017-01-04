372d3126f7b45ddfd64e1438782183b86e304088
Ondrej Zara <ondrej.zara@gmail.com>
2017-01-04 20:53:00 +0100

    http reporter

--- reporter/http.js
+++ reporter/http.js
@@ -5,18 +5,27 @@
 const format = require("../format");
 const http = require("http");
 const https = require("https");
+const url = require("url");
 
 exports.run = function(failures, successes, config) {
 	if (!failures.length) { return; }
 
-	let provider = (config.url.match(/^https/i) ? https : http);
+	let data = [];
 
-	
-	console.log("Failing probes:");
 	failures.forEach(failure => {
-		let lines = format.result(failure, config.verbose);
-		lines.forEach(line => console.log(`\t${line}`));
-		console.log("");
+		let lines = format.result(failure, false);
+		data = data.concat(lines);
 	});
+
+	let provider = (config.url.match(/^https/i) ? https : http);
+	let u = `${config.url}${encodeURIComponent(data.join(", "))}`;
+	let options = url.parse(u);
+	options.method = config.method;
+	let request = provider.request(options, response => {
+		console.log(response);
+	});
+
+	request.on("error", e => console.error(e));
+	request.end();
 }
 
